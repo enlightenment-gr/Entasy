7d05d4bf65ae83fcdecdcc1ee2ba217cb202e0d8
godfath3r <galatsanosp@gmail.com>
2014-07-11 22:20:15 +0300

    make all ui creation functions void and move callbacks to main.

--- src/bin/entasy_ui.c
+++ src/bin/entasy_ui.c
@@ -53,24 +53,22 @@ ent_create_control_buttons() {
        evas_object_smart_callback_add(buttonPlay, "clicked", ent_play, NULL);
        evas_object_smart_callback_add(buttonPause, "clicked", ent_pause, NULL);
        evas_object_smart_callback_add(buttonStop, "clicked", ent_stop, NULL);
-       ent_load_file_list(entUI.tracklist,NULL,NULL);
        evas_object_smart_callback_add(buttonPrev, "clicked", ent_prev_item_play, entUI.tracklist);
        evas_object_smart_callback_add(buttonNext, "clicked", ent_next_item_play, entUI.tracklist);
 }
 
-Evas_Object*
+void
 ent_create_flipper() {
-     // Create and setup the flipbox (or "flipper")
-       Evas_Object *flipbox;
+     // Create and setup the flipper)
 
-       flipbox = elm_flip_add(entUI.window);
-     evas_object_resize(flipbox, 200, 200);
-     elm_table_pack(entUI.controls, flipbox, 0, 0, 6, 6);
-     evas_object_show(flipbox);
+     entUI.flipper = elm_flip_add(entUI.window);
+     evas_object_resize(entUI.flipper, 200, 200);
+     elm_table_pack(entUI.controls, entUI.flipper, 0, 0, 6, 6);
+     evas_object_show(entUI.flipper);
 
-     elm_flip_interaction_set(flipbox, ELM_FLIP_INTERACTION_PAGE);
-     elm_flip_interaction_direction_hitsize_set(flipbox, ELM_FLIP_DIRECTION_LEFT, 1);
-     elm_flip_interaction_direction_hitsize_set(flipbox, ELM_FLIP_DIRECTION_RIGHT, 1);
+     elm_flip_interaction_set(entUI.flipper, ELM_FLIP_INTERACTION_PAGE);
+     elm_flip_interaction_direction_hitsize_set(entUI.flipper, ELM_FLIP_DIRECTION_LEFT, 1);
+     elm_flip_interaction_direction_hitsize_set(entUI.flipper, ELM_FLIP_DIRECTION_RIGHT, 1);
 
      // Creating the album cover
      Evas_Object *cover;
@@ -78,21 +76,19 @@ ent_create_flipper() {
      elm_photo_size_set(cover,200);
      elm_photo_fill_inside_set(cover, EINA_TRUE);
      elm_photo_file_set(cover, "nullcover.jpg");
-     elm_object_part_content_set(flipbox, "front", cover);
+     elm_object_part_content_set(entUI.flipper, "front", cover);
      evas_object_show(cover);
 
      // Creating the lyrics textbox
      Evas_Object *lyricstext;
      lyricstext = elm_entry_add(entUI.window);
      elm_entry_entry_set (lyricstext,"No Lyrics Found!");
-     elm_object_part_content_set(flipbox, "back", lyricstext);
+     elm_object_part_content_set(entUI.flipper, "back", lyricstext);
      evas_object_show(lyricstext);
 
      // Setup the callbacks
-     evas_object_smart_callback_add(cover, "clicked", ent_flip_panel, flipbox);
-       evas_object_smart_callback_add(lyricstext, "clicked", ent_flip_panel, flipbox);
-
-     return flipbox;
+     evas_object_smart_callback_add(cover, "clicked", ent_flip_panel, entUI.flipper);
+     evas_object_smart_callback_add(lyricstext, "clicked", ent_flip_panel, entUI.flipper);
 }
 
 void
@@ -122,14 +118,9 @@ ent_create_sliders() {
    evas_object_size_hint_align_set(entUI.volumeSlider, EVAS_HINT_FILL, EVAS_HINT_FILL);
    elm_table_pack(entUI.controls, entUI.volumeSlider, 10, 1, 1, 1);
    evas_object_show(entUI.volumeSlider);
-
-   // Setting up callbacks
-   evas_object_smart_callback_add(entUI.volumeSlider, "changed", ent_change_volume, NULL);
-   evas_object_smart_callback_add(entUI.timeSlider, "changed", ent_change_time, NULL);
-   evas_object_smart_callback_add(entUI.emotion, "position_update", ent_update_time, entUI.timeSlider);
 }
 
-Evas_Object *
+void
 ent_create_directory() {
       Evas_Object *o, *folder;
 
@@ -140,30 +131,25 @@ ent_create_directory() {
       elm_table_pack(entUI.controls, o, 6, 6, 1, 1);
       evas_object_show(o);
 
-      folder = elm_entry_add(entUI.window);
-      evas_object_size_hint_weight_set(folder, EVAS_HINT_EXPAND, 0.0);
-      evas_object_size_hint_align_set(folder, EVAS_HINT_FILL, 0.5);
-      elm_entry_single_line_set(folder, EINA_TRUE);
-      elm_entry_scrollable_set(folder, EINA_TRUE);
-      elm_scroller_policy_set(folder, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_OFF);
-      elm_object_text_set(folder, config.directory);
-      //elm_box_pack_end(entUI.window, folder);
-      elm_table_pack(entUI.controls, folder, 6, 7, 5, 1);
-      evas_object_show(folder);
-
-      return folder;
+      entUI.directory = elm_entry_add(entUI.window);
+      evas_object_size_hint_weight_set(entUI.directory, EVAS_HINT_EXPAND, 0.0);
+      evas_object_size_hint_align_set(entUI.directory, EVAS_HINT_FILL, 0.5);
+      elm_entry_single_line_set(entUI.directory, EINA_TRUE);
+      elm_entry_scrollable_set(entUI.directory, EINA_TRUE);
+      elm_scroller_policy_set(entUI.directory, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_OFF);
+      elm_object_text_set(entUI.directory, config.directory);
+      //elm_box_pack_end(entUI.window, entUI.directory);
+      elm_table_pack(entUI.controls, entUI.directory, 6, 7, 5, 1);
+      evas_object_show(entUI.directory);
 }
 
-Evas_Object*
+void
 ent_create_tracklist() {
-    Evas_Object *tracklist;
 
-    tracklist = elm_list_add(entUI.window);
-      elm_list_mode_set(tracklist, ELM_LIST_LIMIT);
-      evas_object_size_hint_weight_set(tracklist, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-      evas_object_size_hint_align_set(tracklist, EVAS_HINT_FILL, EVAS_HINT_FILL);
-      elm_table_pack(entUI.controls, tracklist, 6, 2, 6, 4);
-      evas_object_show(tracklist);
-
-      return tracklist;
+      entUI.tracklist = elm_list_add(entUI.window);
+      elm_list_mode_set(entUI.tracklist, ELM_LIST_LIMIT);
+      evas_object_size_hint_weight_set(entUI.tracklist, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+      evas_object_size_hint_align_set(entUI.tracklist, EVAS_HINT_FILL, EVAS_HINT_FILL);
+      elm_table_pack(entUI.controls, entUI.tracklist, 6, 2, 6, 4);
+      evas_object_show(entUI.tracklist);
 }
--- src/bin/entasy_ui.h
+++ src/bin/entasy_ui.h
@@ -2,7 +2,7 @@
 
 void ent_create_control_buttons();
 void ent_create_sliders();
-Evas_Object * ent_create_flipper();
-Evas_Object * ent_create_directory();
-Evas_Object * ent_create_tracklist();
+void ent_create_flipper();
+void ent_create_directory();
+void ent_create_tracklist();
 
